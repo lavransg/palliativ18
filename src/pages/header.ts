841e34000c505be7c7b163ded97eb37154495e74
Lavrans Grønseth <lavrans.gronseth@gmail.com>
2019-07-16 11:22:52 +0200

    fixed an issue where navigation buttons on header bar had the onclick on the icon, not a containing box, which made the click area too small

--- src/pages/header.ts
+++ src/pages/header.ts
@@ -7,25 +7,8 @@ import { SearchPage } from './search/search';
 @Component({
   selector: 'header-bar',
   inputs: ['title: title',"frontpage: frontpage"],
-  template:
-  `
-  <ion-row class="menu-header">
-    <ion-col col-2></ion-col>
-    <ion-col col-8 class="title"><b>{{title}}</b></ion-col>
-    <ion-col col-2></ion-col>
-  </ion-row>
-
-  <div class="header-fixed">
-    <ion-row class="header-shadow">
-      <ion-col col-2>
-        <ion-icon name="arrow-back" *ngIf="!frontpage" (click)="backButtonClicked()"></ion-icon>
-        <ion-icon name="ios-information-circle-outline" *ngIf="frontpage" (click)="infoButtonClicked()"></ion-icon>
-      </ion-col>
-      <ion-col col-8><b>{{title}}</b></ion-col>
-      <ion-col col-2><ion-icon name="search" (click)=searchButtonClicked()></ion-icon></ion-col>
-    </ion-row>
-  </div>
-  `
+  templateUrl: 'header.html'
+  
 })
 export class HeaderBar {
 
